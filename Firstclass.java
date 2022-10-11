817cce194c1bd683c1f57165a3d25dbd7aa65b8e
ISiriGit <sivasirisha93@gmail.com>
2022-10-11 21:21:38 +0530

    my first commit

---
+++ Firstclass.java
@@ -0,0 +1,11 @@
+package firstApp;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+System.out.println("this is my first app after a longgg time");
+int i=10;
+System.out.println("the value of i is :"+i);
+	}
+
+}
